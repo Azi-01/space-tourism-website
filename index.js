8df22ec3679165e511c2457088028305f4f55360
Azan <azanrashid26@gmail.com>
2022-03-24 17:47:55 +0500

    Fixed HTML issues

--- index.js
+++ index.js
@@ -100,13 +100,13 @@ page_links.forEach(page_link => {
                 else if(section.classList.contains("hide")){
                     section.classList.remove("hide");
                     if(window.screen.width < 720){
-                        document.body.childNodes[1].style.backgroundImage = `url(./assets/${page_link.id}/background-${page_link.id}-mobile.jpg)`; 
+                        document.body.childNodes[1].style.backgroundImage = `url(./assets/${page_link.id.slice(0,-1)}/background-${page_link.id.slice(0,-1)}-mobile.jpg)`; 
                     }
                     else if(window.screen.width < 1032){
-                        document.body.childNodes[1].style.backgroundImage = `url(./assets/${page_link.id}/background-${page_link.id}-tablet.jpg)`;
+                        document.body.childNodes[1].style.backgroundImage = `url(./assets/${page_link.id.slice(0,-1)}/background-${page_link.id.slice(0,-1)}-tablet.jpg)`;
                     }
                     else{
-                        document.body.childNodes[1].style.backgroundImage = `url(./assets/${page_link.id}/background-${page_link.id}-desktop.jpg)`;
+                        document.body.childNodes[1].style.backgroundImage = `url(./assets/${page_link.id.slice(0,-1)}/background-${page_link.id.slice(0,-1)}-desktop.jpg)`;
                     }
                 }
 
